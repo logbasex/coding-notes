5b5e43177816e80adf7fdb8e32432b05bb5f326f
logbasex <contact.hoducanh@gmail.com>
2022-04-08 23:40:58 +0700

    mongdb backup script.

--- web-development/backend/database/mongodb/mongo-eko.sh
+++ web-development/backend/database/mongodb/mongo-eko.sh
@@ -19,22 +19,26 @@ if [[ $option == "0" ]]; then
     exit 1;
 fi
 
-read -p "$(echo -e $BOLD$YELLOW"Which is database you wanna backup (Press 1 for eko_test, 2 for eko_staging, 3 for custom URI):"$RESET)"$'\n> ' mongo_server
-echo
+if [[ $option == "1" || $option == "3" ]]; then
+    read -p "$(echo -e $BOLD$YELLOW"Which is database you wanna backup (Press 1 for eko_test, 2 for eko_staging, 3 for custom URI):"$RESET)"$'\n> ' mongo_server
+    echo
+
+    read -p "$(echo -e $BOLD$YELLOW"Which is database colllections to exclude (Split collection by comma)?"$RESET)"$'\n> ' exclude_collections
+    echo
+
+    read -p "$(echo -e $BOLD$CYAN"Please enter your backup folder (Which is default stored in /tmp/mongodb-backups):"$RESET)"$'\n> ' folder
+    echo
+fi
 
 if [[ $mongo_server == "1" ]]; then
-    mongo_server="mongodb://192.168.5.21:27017"
+    mongo_server="mongodb://192.168.5.21:27017/eko_test"
 elif [[ $mongo_server == "2" ]]; then
     mongo_server="mongodb+srv://ffclique-staging:aYKjVVddPLKUmYCK@clique-staging.c5mix.mongodb.net/eko_staging"
 fi
 
-read -p "$(echo -e $BOLD$YELLOW"Which is database colllections to exclude (Split collection by comma)?"$RESET)"$'\n> ' exclude_collections
-echo
 
-BACKUP_FILE_NAME=$(echo $mongo_server| cut -d "/" -f3 | cut -c -20)"__""$(date +%Y-%m-%d)"".gz"
-
-read -p "$(echo -e $BOLD$CYAN"Please enter your backup folder (Which is default stored in /tmp/mongodb-backups):"$RESET)"$'\n> ' folder
-echo
+BACKUP_FILE_NAME=$(echo $mongo_server| cut -d "/" -f4 | cut -c -20)"__""$(date +%Y-%m-%d)"".gz"
+BACKUP_DB_NAME=$(echo $mongo_server| cut -d "/" -f4)
 
 split_on_commas() {
     local WORD_LIST=($(echo $exclude_collections | tr "," "\n"))
@@ -55,7 +59,7 @@ backup() {
     echo "##################################"
 
     local excluded_collections=$(split_on_commas)
-    sudo docker exec $MONGODB_CONTAINER_ID /bin/bash -c "exec mongodump --uri=$mongo_server --numParallelCollections=$BACKUP_COLLECTION_PARALLELISM --archive=$BACKUP_FILE_NAME $excluded_collections"
+    sudo docker exec $MONGODB_CONTAINER_ID /bin/bash -c "exec mongodump --uri=$mongo_server --db=$BACKUP_DB_NAME --numParallelCollections=$BACKUP_COLLECTION_PARALLELISM --archive=$BACKUP_FILE_NAME $excluded_collections"
 
     LAST_STATUS=$(echo $?)
 
@@ -69,7 +73,7 @@ backup() {
         fi
 
         echo "##################################"
-        echo "Backup completed at '`date`' and located in $folder'"
+        echo "Backup completed at '`date`' and stored in file '$folder$BACKUP_FILE_NAME'"
         echo "##################################"
     else
         echo "Errors were occurred while dumping your DB."
@@ -78,44 +82,50 @@ backup() {
 
 
 restore() {
-    if [[ $0 == "2" ]]; then
-        read -p "$(echo -e $BOLD$YELLOW"Please enter BACKUP_FILE's absolute path:"$RESET)"$'\n' backup_file_path
+    if [[ $1 == "2" ]]; then
+        read -p "$(echo -e $BOLD$YELLOW"Please enter BACKUP_FILE's absolute path:"$RESET)"$'\n> ' backup_file_path
+
+        read -p "$(echo -e $BOLD$YELLOW"Please enter BACKUP_DB_NAME (Ex: eko_test):"$RESET)"$'\n> ' dump_db_name
         echo
     fi
 
-    read -p "$(echo -e $BOLD$YELLOW"Please enter BACKUP_DB_NAME (Default: eko_test):"$RESET)"$'\n>' backup_db_name
+
+    read -p "$(echo -e $BOLD$YELLOW"Which is database name to restore dump backup? (Default: eko_dev):"$RESET)"$'\n> ' restore_db_name
     echo
 
-    read -p "$(echo -e $BOLD$YELLOW"Please enter RESTORE_DB_NAME (Default: eko_dev):"$RESET)"$'\n>' restore_db_name
-    echo
+    if [[ $1 == "3" ]]; then
+        dump_db_name=$BACKUP_DB_NAME;
+    fi
 
-    SOURCE_DB_NAME=$backup_db_name".*"
-    TARGET_DB_NAME=$restore_db_name".*"
+    SOURCE_DB_NAME="$dump_db_name"".*"
+    TARGET_DB_NAME="$restore_db_name"".*"
     echo "##################################"
     echo "Restore will start at '`date`' for database: '$SOURCE_DB_NAME'"
     echo "##################################"
 
-    sudo docker exec -i $MONGODB_CONTAINER_ID /bin/bash -c 'mongorestore --archive --drop --nsFrom="$SOURCE_DB_NAME" --nsTo="$TARGET_DB_NAME"' < $DEFAULT_BACKUP_PARENT_DIR/$BACKUP_FILE_NAME
+    if [[ $1 == "3" ]]; then
+        backup_file_path=$DEFAULT_BACKUP_PARENT_DIR/$BACKUP_FILE_NAME
+    fi
 
+    sudo docker exec -i $MONGODB_CONTAINER_ID /bin/bash -c "mongorestore --archive --drop --nsFrom="$SOURCE_DB_NAME" --nsTo="$TARGET_DB_NAME"" < $backup_file_path
     echo "##################################"
     echo "Restore completed at '`date`' to '$TARGET_DB_NAME"
     echo "##################################"
 }
 
 
-if [ -x "$(command -v docker)" ]; then
+if [ -x $(command -v docker) ]; then
     MONGODB_CONTAINER_ID=$(sudo -S docker ps -q  --filter ancestor=mongo -f status=running)
-    echo $MONGODB_CONTAINER_ID
 
     if [[ "${MONGODB_CONTAINER_ID}" ]]; then
 
         if [[ $option == "1" ]]; then
             backup
         elif [[ $option == "2" ]]; then
-            restore
+            restore 2
         elif [[ $option == "3" ]]; then
             backup
-            restore
+            restore 3
         fi
     else
         echo "MongoDB's container isn't running. Please check!"
