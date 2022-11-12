9c735a5dda5aef86daceaaa06e18c7b930dfc29d
logbasex <contact.hoducanh@gmail.com>
2022-11-12 10:35:10 +0700

    update install script.

--- web-development/linux/setup-scripts/install.sh
+++ web-development/linux/setup-scripts/install.sh
@@ -43,138 +43,143 @@ if [[ $EUID -ne 0 ]]; then
 else
 #        echo "connect to wifi"
 #        nmcli d wifi connect Bluebottle password Blu3B0ttl3
-  runAptGetUpdate
+     runAptGetUpdate
+    echo "Install common packages"
+    sudo apt install snap python3-pip -y
+    sudo apt install dialog -y
 
-	echo "Install common packages"
-	apt install snap python3-pip -y
-				apt install dialog
 	cmd=(dialog --separate-output --checklist "Please select things you want to install" 22 76 16)
-	options=(1 "Google Chrome" on    # any option can be set to default to "on"
-		 2 "Intellij Ultimate" on
-		 3 "DataGrip" on
-		 4 "Communications" on
-		 5 "Office" on
-		 6 "SublimeText" on
-		 7 "Yarn" on
-		 8 "NodeJs" on
-		 9 "JDK 8" on
-		 10 "Docker" on
-		 11 "MySQL 5.6" on
-		 12 "MongoDB" on
-		 13 "RabbitMQ" on
-		 14 "Elasticsearch" on
-		 15 "Backup Eko Test DB" on
-		 16 "Handy Tools" off)
+	options=(
+	    1 "Google Chrome" on    # any option can be set to default to "on"
+        2 "Intellij Ultimate" on
+        3 "LibreOffice" on
+        4 "SublimeText" on
+        5 "NodeJs & Yarn" on
+        6 "JDK 8" on
+        7 "Docker & docker-compose" on
+        8 "MongoDB" on
+        9 "Elasticsearch" on
+        10 "Postman" on
+        11 "Redis" on
+        12 "Skype" on
+        13 "Mattermost" on
+        14 "SSH server" on
+        15 "Clipboard manager" on
+        15 "Vietnamese IME (ibus-bamboo)" on
+        16 "Handy Tools" on
+    )
+
 	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 	clear
 	for choice in $choices
 	do
-			case $choice in
+        case $choice in
 		1)
-				echo "Google Chrome"
-					if command -v google-chrome ; then
-						echo "Google Chrome is already installed."
-					else
-						wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-						sudo dpkg -i google-chrome-stable_current_amd64.deb
-					fi
-				;;
+            echo "Google Chrome"
+                if command -v google-chrome ; then
+                    echo "Google Chrome is already installed."
+                else
+                    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                    sudo dpkg -i google-chrome-stable_current_amd64.deb
+                fi
+            ;;
 		2)
-				echo "Intellij Ultimate"
-				sudo snap install intellij-idea-ultimate --classic
-				;;
+            echo "Intellij Ultimate"
+            sudo snap install intellij-idea-ultimate --classic
+            ;;
 		3)
-				echo "DataGrip"
-				sudo snap install datagrip --classic
-				;;
+            echo "LibreOffice"
+            sudo apt install libreoffice -y
+            ;;
 		4)
-				echo "Communications"
-				sudo snap install slack --classic
-				sudo snap install skype --classic
-				;;
+            echo "SublimeText"
+            sudo snap install sublime-text --classic
+            ;;
 		5)
-				echo "Office"
-				sudo apt install libreoffice -y
-				;;
+            echo "NodeJs & Yarn"
+            sudo apt install git -y
+            export NVM_DIR="$HOME/.nvm" && (
+            git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+            cd "$NVM_DIR"
+            git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+            ) && \. "$NVM_DIR/nvm.sh"
+
+            # latest version
+            nvm install node
+            nvm use node
+
+            npm install -g yarn
+            ;;
 		6)
-				echo "SublimeText"
-				sudo snap install sublime-text --classic
-				;;
+            echo "JDK 8"
+            sudo apt install openjdk-8-jdk openjdk-8-jdk-headless -y
+            ;;
 		7)
-				echo "Yarn"
-				sudo apt install --no-install-recommends yarn -y
-				;;
+            echo "Docker & docker-compose"
+            sudo snap install docker
+            sudo apt install docker-ce -y && sudo usermod -aG docker $USER && sudo chmod 666 /var/run/docker.sock
+
+            sudo apt install docker-compose
+            ;;
 		8)
-				echo "NodeJs"
-				sudo apt install git -y
-				export NVM_DIR="$HOME/.nvm" && (
-				git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-				cd "$NVM_DIR"
-				git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-				) && \. "$NVM_DIR/nvm.sh"
-
-				nvm install v14.17.0
-				nvm use v14.17.0
-				;;
+            echo "MongoDB"
+            sudo docker run -d --name=mongo --restart=always -p=27017:27017 mongo:latest
+            ;;
 		9)
-				echo "JDK 8"
-				sudo apt install openjdk-8-jdk openjdk-8-jdk-headless -y
-				;;
-		10)
-				echo "Docker"
-				sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-				curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-				sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-				sudo apt update -y
-				sudo apt install docker-ce -y && sudo usermod -aG docker $USER && sudo chmod 666 /var/run/docker.sock
-
-				pip3 install docker-compose
-				;;
-		11)
-				echo "MySQL 5.6"
-				mkdir -p $HOME/docker/mysql56
-				sudo docker pull mysql:5.6
-
-				sudo docker run -p 3306:3306 --name mysql56 \
-					--restart always \
-					-v $PWD/conf:/etc/mysql \
-					-v $PWD/logs:/var/log/mysql \
-					-v $PWD/data:/var/lib/mysql \
-					-e MYSQL_ROOT_PASSWORD=123456 \
-					-d mysql:5.6 \
-					--character-set-server=utf8mb4 \
-					--collation-server=utf8mb4_unicode_ci
-				;;
-		12)
-				echo "MongoDB"
-				sudo docker run -d --name=mongo44 --restart=always -p=27017:27017 mongo:latest
-				;;
-		13)
-				echo "RabbitMQ"
-				docker run -d --hostname my-rabbit --name some-rabbit --restart=always -p 4369:4369 -p 5671:5671 -p 5672:5672 -p 15672:15672 rabbitmq:3-management
-				;;
-		14)
-				echo "ElasticSearch"
-				curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-				echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-				sudo apt update -y
-				sudo apt install elasticsearch -y
-
-				echo "-Xms1G" > /etc/elasticsearch/jvm.options.d/user.options
-				echo "-Xmx1G" >> /etc/elasticsearch/jvm.options.d/user.options
-
-				sudo systemctl start elasticsearch.service
-				sudo systemctl enable elasticsearch.service
-				;;
-
-		15)
-				echo "Backup Eko Test DB"
-								 docker exec mongo44 /bin/bash -c 'exec mongodump --host 192.168.5.21:27017 --db eko_test --archive | mongorestore --archive --nsFrom="eko_test.*" --nsTo="eko_dev.*" --drop'
-				;;
-		16)
-				echo "Handy Tools"
-				;;
-			esac
+            echo "ElasticSearch"
+            sudo docker run --name elasticsearch \
+             -p 9200:9200 \
+             -e discovery.type=single-node \
+             -e ES_JAVA_OPTS="-Xms1g -Xmx1g" \
+             -e xpack.security.enabled=false \
+             --restart=always \
+             -d docker.elastic.co/elasticsearch/elasticsearch:7.13.2
+            ;;
+        10)
+            echo "Postman"
+            sudo snap install postman --classic
+            ;;
+        11)
+            echo "Redis"
+            sudo docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 --restart=always redis/redis-stack:latest
+            ;;
+        12)
+            echo "Skype"
+            sudo snap install skype --classic
+            ;;
+        13)
+            echo "Mattermost"
+            sudo snap install mattermost-desktop
+            ;;
+        14)
+            echo "SSH server"
+            sudo apt install -y openssh-server
+            ;;
+        15)
+            echo "Clipboard manager"
+            sudo apt install copyq
+            ;;
+        16)
+            echo "Vietnamese Input Method Editor (ibus-bamboo)"
+            sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
+            sudo apt update
+            sudo apt install ibus ibus-bamboo --install-recommends
+            ibus restart
+            # Đặt ibus-bamboo làm bộ gõ mặc định
+            env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo')]"
+            ;;
+		17)
+            echo "Handy Tools (htop, peek, jq..)"
+            # htop
+            sudo apt install htop
+            # peek
+            sudo apt install peek
+            # jq
+            sudo apt install jq
+            # ncdu
+            sudo apt install ncdu
+            ;;
+        esac
 	done
 
 fi
